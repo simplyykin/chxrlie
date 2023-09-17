763d003306bd6263b4cc0c3ac3148e6e2316b41b
charlie <56640971+simplyykin@users.noreply.github.com>
2023-09-16 23:43:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+ const button = document.getElementById('toggle-button');
+        const title = document.getElementById('title');
+        const subtitle = document.getElementById('subtitle');
+        const content = document.getElementById('content');
+        const audio = new Audio('moonlight.mp3'); // Replace with your audio file
+        audio.volume = 0.2;
 
+        button.addEventListener('click', function() {
+            button.style.display = 'none'; // Hide the button
+            title.style.display = 'block'; // Show the title
+            subtitle.style.display = 'block'; // Show the subtitle
+            content.style.display = 'block'; // Show the content
+            audio.play(); // Autoplay the audio
+        });
